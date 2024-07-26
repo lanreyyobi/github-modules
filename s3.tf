1a2d1a8255686813c96d557b46b0fa7869571295
WemaDevOps <152240864+Wemadevops@users.noreply.github.com>
2024-07-25 20:32:19 -0400

    Wd102 47 (#2)
    * chore: S3 backend for terraform
    
    Contributing-to:https://wemadevops.atlassian.net/jira/software/projects/DEV/boards/2?selectedIssue=WD102-47
    
    * feat: Bucket notification added
    
    Contributing-to:https://wemadevops.atlassian.net/jira/software/projects/DEV/boards/2?selectedIssue=WD102-47

--- s3.tf
+++ s3.tf
@@ -22,3 +22,20 @@ resource "aws_sns_topic" "topic" {
   name              = var.topic_name
 }
 
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.topic.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  endpoint  = "info@wemadevops.com"
+  protocol  = "email"
+  topic_arn = aws_sns_topic.topic.arn
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+env = "prod"
+topic_name = "wema"
+my_bucket = "devops"
+team = "cloud"
